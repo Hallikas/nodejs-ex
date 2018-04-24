6bf0dbc6b6bdd07922c5c75bb06528bb1f29499f
Sami-Pekka Hallikas <semi@hallikas.com>
2018-04-24 11:06:55 +0300

    retry 5...

--- server.js
+++ server.js
@@ -16,9 +16,11 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
+  hostname = os.hostname();
+  uptime = os.uptime();
+    
   res.render('index.html', { 
-    hostname = os.hostname(),
-    uptime = os.uptime()
+    hostname, uptime
   });
 });
 
